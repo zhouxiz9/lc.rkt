aed58afe430058f39b548f0e34a023295273b3b7
mar444 <mark.zhouxili@gmail.com>
2017-12-12 15:33:45 -0800

    support alpha reduction

--- src/evaluator.rkt
+++ src/evaluator.rkt
@@ -5,25 +5,54 @@
 (require "lexer.rkt")
 (require "parser.rkt")
 
+(define ALPHA "α")
+(define BETA "β")
+
+(define REDUCTION-TYPE '())
+
+
 (define (reduce ast)
-  (cond ((application? ast)
-         (let ((lhs (application-lhs ast))
-               (rhs (application-rhs ast)))
-           (cond ((abstraction? lhs)
-                  (subst (abstraction-body lhs) (abstraction-param lhs) rhs))
-                 ((application? lhs)
-                  (make-application (reduce lhs) rhs))
-                 ((application? rhs)
-                  (make-application lhs (reduce rhs)))
-                 (else ast))))
-        ((abstraction? ast)
-         (make-abstraction (abstraction-param ast) (reduce (abstraction-body ast))))
-        (else ast)))
+  (cond ((need-alpha-reduction? ast)
+         (set! REDUCTION-TYPE ALPHA)
+         (letrec ((update-id
+                   (lambda (old-id)
+                     (let ((new-id (gen-new-id old-id)))
+                       (if (member new-id (get-ids ast))
+                           (update-id new-id)
+                           new-id))))
+                  (update-ast
+                   (lambda (old-ast ast)
+                     (cond ((application? old-ast)
+                            (make-application (update-ast (application-lhs old-ast) ast)
+                                              (update-ast (application-rhs old-ast) ast)))
+                           ((abstraction? old-ast)
+                            (let ((param (abstraction-param old-ast)))
+                              (if (id-duplicate? param ast)
+                                  (let ((new-param (make-identifier (update-id (identifier-value param)))))
+                                    (make-abstraction new-param (subst (abstraction-body old-ast) param new-param)))
+                                  old-ast)))
+                           (else old-ast)))))
+           (update-ast ast ast)))                             
+        (else
+         (set! REDUCTION-TYPE BETA)
+         (cond ((application? ast)
+                (let ((lhs (application-lhs ast))
+                      (rhs (application-rhs ast)))
+                  (cond ((abstraction? lhs)
+                         (subst (abstraction-body lhs) (abstraction-param lhs) rhs))
+                        ((application? lhs)
+                         (make-application (reduce lhs) rhs))
+                        ((application? rhs)
+                         (make-application lhs (reduce rhs)))
+                        (else ast))))
+               ((abstraction? ast)
+                (make-abstraction (abstraction-param ast) (reduce (abstraction-body ast))))
+               (else ast)))))
 
 
 (define (subst e v e1)
   (cond ((identifier? e)
-         (if (equal? (identifier-value e) (identifier-value v)) e1 e))
+         (if (equal-identifier? e v) e1 e))
         ((application? e)
          (make-application (subst (application-lhs e) v e1)
                            (subst (application-rhs e) v e1)))
@@ -32,7 +61,7 @@
                (body (abstraction-body e))
                (fve (map identifier-value (freevars e)))
                (fve1 (map identifier-value (freevars e1))))
-           (cond ((equal? (identifier-value param) (identifier-value v)) e)
+           (cond ((equal-identifier? param v) e)
                  ((not (member (identifier-value param) fve1))
                   (make-abstraction param (subst body v e1)))
                  (else
@@ -56,6 +85,29 @@
         (else e)))
 
 
+(define (get-ids ast)
+  (cond ((application? ast)
+         (append (get-ids (application-lhs ast))
+                 (get-ids (application-rhs ast))))
+        ((abstraction? ast)
+         (append (list (identifier-value (abstraction-param ast)))
+                 (get-ids (abstraction-body ast))))
+        (else '())))
+
+                                        
+(define (gen-new-id old-id)
+  (symbol->string (gensym old-id)))
+
+(define (need-alpha-reduction? ast)
+  (check-duplicates (get-ids ast)))
+
+(define (id-duplicate? id ast)
+  (> (count (lambda (i) (equal? i (identifier-value id))) (get-ids ast)) 1))
+                                         
+(define (equal-identifier? i1 i2)
+  (equal? (identifier-value i1) (identifier-value i2)))
+
+
 (define (print-reduce-result str)
   (let ((result (reduce (parse (lexer str)))))
     (print (ast-node->string result))))
@@ -75,11 +127,9 @@
 
          
 (define (ans exp)
-  (ast-node->string
-   (if (string? exp)
-       (reduce-str exp)
-       exp)))
-
+  (let ((result  (ast-node->string (if (string? exp) (reduce-str exp) exp))))
+    (string-append (if (equal? REDUCTION-TYPE ALPHA) ALPHA BETA) " > " result)))
+      
 
 ;; helper functions
 (define (list-union l1 l2)
@@ -94,3 +144,8 @@
 
 
 (test)
+
+;; example
+
+;; (ans "(\\x.(\\s.(s z x))) (\\z.(z s))")
+;; "β > (λs158871.(s158871 z))"
